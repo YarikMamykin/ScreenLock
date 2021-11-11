99049cc94351d7e28024613ebdf3bb16cbd2a975
YarikM <ymamykin@gmail.com>
2021-11-11 23:49:24 +0200

    Update helpers with dontkillme func

--- helpers/helpers.c
+++ helpers/helpers.c
@@ -2,10 +2,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-void
-die(const char *errstr, ...)
-{
+void die(const char *errstr, ...) {
 	va_list ap;
 
 	va_start(ap, errstr);
@@ -14,3 +14,22 @@ die(const char *errstr, ...)
 	exit(1);
 }
 
+void dontkillme(void) {
+	FILE *f;
+	const char oomfile[] = "/proc/self/oom_score_adj";
+
+	if (!(f = fopen(oomfile, "w"))) {
+		if (errno != ENOENT)
+			die("slock: fopen %s: %s\n", oomfile, strerror(errno));
+	}
+
+	fprintf(f, "%d", OOM_SCORE_ADJ_MIN);
+
+	if (fclose(f)) {
+		if (errno == EACCES)
+			die("slock: unable to disable OOM killer. "
+			    "Make sure to suid or sgid slock.\n");
+		else
+			die("slock: fclose %s: %s\n", oomfile, strerror(errno));
+	}
+}
--- helpers/helpers.h
+++ helpers/helpers.h
@@ -1,3 +1,8 @@
 #pragma once
+#ifdef __linux__
+#include <fcntl.h>
+#include <linux/oom.h>
+#endif
 
 void die(const char *errstr, ...);
+void dontkillme(void);
