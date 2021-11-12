00b1c30ab0c09efc697a8de25fa66f850a7a1090
YarikM <ymamykin@gmail.com>
2021-11-12 13:05:18 +0200

    Update locking_window with screen locking

---
+++ window/locking_window.c
@@ -0,0 +1,72 @@
+#include <locking_window.h>
+#include <stdlib.h>
+
+const char* init_xlib(struct locking_window* lw) {
+
+	if (!(lw->dpy = XOpenDisplay(NULL))) {
+		return "slock: cannot open display\n";
+	}
+
+	/* check for Xrandr support */
+	lw->rr.active = XRRQueryExtension(lw->dpy, &lw->rr.evbase, &lw->rr.errbase);
+
+	/* get number of screens in display "dpy" and blank them */
+	lw->nscreens = ScreenCount(lw->dpy);
+	printf("Number of screens: %d\n", lw->nscreens);
+
+	if (!(lw->locks = calloc(lw->nscreens, sizeof(struct lock *)))) {
+		return "slock: out of memory\n";
+	}
+
+	return NULL;
+}
+
+void free_xlib(struct locking_window* lw) {
+
+	XCloseDisplay(lw->dpy);
+
+	for(int i = 0; i < lw->nscreens; ++i) {
+		free(lw->locks[i]);
+	}
+
+	free(lw->locks);
+	free(lw);
+}
+
+const char* lockscreen(struct locking_window* lw, int screen) {
+	lw->locks[screen] = malloc(sizeof(struct lock));
+	RootWindow(lw->dpy, lw->locks[screen]->screen);
+	return NULL;
+}
+
+const char* lock_all_screens(struct locking_window* lw) {
+	for(int i = 0; i < lw->nscreens; ++i) {
+		const char* err_msg = lockscreen(lw, i);
+		if(err_msg != NULL) {
+			return err_msg;
+		}
+	}
+
+	return NULL;
+}
+
+const char* run_ui() {
+	
+	struct locking_window* lw = (struct locking_window*)calloc(1, sizeof(struct locking_window));
+	const char* err_msg = init_xlib(lw);
+
+	if(err_msg != NULL) {
+		free_xlib(lw);
+		return err_msg;
+	}
+
+	err_msg = lock_all_screens(lw);
+
+	if(err_msg != NULL) {
+		free_xlib(lw);
+		return err_msg;
+	}
+
+	free_xlib(lw);
+	return NULL;
+}
--- window/locking_window.h
+++ window/locking_window.h
@@ -1,1 +1,41 @@
 #pragma once
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <X11/extensions/Xrandr.h>
+#include <X11/keysym.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <config.h>
+
+struct lock {
+	int screen;
+	Window root, win;
+	Pixmap pmap;
+	unsigned long colors[NUMCOLS];
+};
+
+struct xrandr {
+	int active;
+	int evbase;
+	int errbase;
+};
+
+struct locking_window {
+
+	struct xrandr rr;
+	struct lock **locks;
+	Display *dpy;
+	int nscreens;
+
+};
+
+const char* init_xlib(struct locking_window* lw);
+void free_xlib(struct locking_window* lw);
+const char* lock_all_screens(struct locking_window* lw);
+const char* run_ui();
