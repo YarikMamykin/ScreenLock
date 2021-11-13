710a890a605ec14009565b5e2269bde4b450f404
YarikM <ymamykin@gmail.com>
2021-11-13 23:17:39 +0200

    Update screen locking function

--- window/locking_window.c
+++ window/locking_window.c
@@ -38,8 +38,47 @@ void free_xlib(struct locking_window* lw) {
 }
 
 const char* lockscreen(struct locking_window* lw, int screen) {
-	lw->locks[screen] = malloc(sizeof(struct lock));
-	RootWindow(lw->dpy, lw->locks[screen]->screen);
+	lw->locks[screen] = (struct lock*)calloc(1, sizeof(struct lock));
+	lw->locks[screen]->root = RootWindow(lw->dpy, lw->locks[screen]->screen);
+
+	XSetWindowAttributes wa;
+	wa.override_redirect = 1;
+	wa.background_pixel = 0ul;
+	lw->locks[screen]->win = XCreateWindow(
+		lw->dpy,
+		lw->locks[screen]->root,
+		0,0, XDisplayWidth(lw->dpy, screen), XDisplayHeight(lw->dpy, screen),
+		0, XDefaultDepth(lw->dpy, screen),
+		CopyFromParent,
+		DefaultVisual(lw->dpy, screen),
+		CWOverrideRedirect | CWBackPixel, &wa);
+
+	char curs[] = {0, 0, 0, 0, 0, 0, 0, 0};
+	XColor color;
+	color.red = color.blue = color.green = 0;
+
+	lw->locks[screen]->pmap = XCreateBitmapFromData(lw->dpy, lw->locks[screen]->win, curs, 8, 8);
+	Cursor invisible = XCreatePixmapCursor(lw->dpy, lw->locks[screen]->pmap, lw->locks[screen]->pmap, &color, &color, 0, 0);
+	XDefineCursor(lw->dpy, lw->locks[screen]->win, invisible);
+
+	if(GrabSuccess != XGrabPointer(lw->dpy, lw->locks[screen]->root, False, 
+							ButtonPressMask | ButtonReleaseMask | PointerMotionMask, GrabModeAsync, GrabModeAsync, 
+										None, None, CurrentTime)) {
+		return "Error grabbing mouse";
+	}
+	
+	if(XGrabKeyboard(lw->dpy, lw->locks[screen]->root, True, GrabModeAsync, GrabModeAsync, CurrentTime)) {
+		return "Error grabbing keyboard";
+	}
+
+	if (lw->rr.active)
+		XRRSelectInput(lw->dpy, lw->locks[screen]->win, RRScreenChangeNotifyMask);
+
+	XSelectInput(lw->dpy, lw->locks[screen]->root, SubstructureNotifyMask);
+
+	XSetWindowBackground(lw->dpy, lw->locks[screen]->win, 0ul);
+	XClearWindow(lw->dpy, lw->locks[screen]->win);
+
 	return NULL;
 }
 
